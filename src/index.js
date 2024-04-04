855b9023cb20f4f81184d03fa753bc1bfe3f18bc
hoa1511 <82421063+hoa1511@users.noreply.github.com>
2024-04-04 16:10:18 +0700

    Initial comit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const exphbs = require('express-handlebars');
 const app = express();
 const port = 8080;
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 console.log(path.join(__dirname, 'public'))
 
 // Thiết lập engine template
@@ -15,11 +19,18 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
+  //console.log(req.query.q)
   res.render('news');
-})
-
+});
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send('');
+});
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
